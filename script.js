4dd0905d272d47cf8fd5adc7d480b11d05c721c1
hikmatkhan01 <hikmatk453@gmail.com>
2024-10-14 10:08:55 +0200

    new work

---
+++ script.js
@@ -0,0 +1,16 @@
+var titlle = document.getElementById("khan");
+console.log(titlle);
+
+this.onload = () =>{
+    const titlleLoad = document.getElementById("h11");
+    console.log(titlleLoad.innerHTML);
+
+    var h2title = document.getElementById("h22");
+    console.log(h2title);
+
+    var myPragrap = document.querySelector("#myParagrap");
+     console.log(myPragrap.innerHTML);
+
+     myPragrap.innerHTML = "My New Text";
+     console.log(myPragrap.innerHTML);
+}
